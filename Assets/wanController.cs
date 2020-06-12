533fe1a8353077ac0ba8cde6e76a35ae357be7cc
sho-Uber <shogo1212mail@yahoo.co.jp>
2020-06-13 01:33:55 +0900

    攻撃技の追加 生成がうまくいかない

---
+++ Assets/wanController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wanController : MonoBehaviour {
+    Rigidbody2D rigid;
+    float moveForce = 60.0f;
+    GameObject player;
+
+    // Start is called before the first frame update
+    void Start () {
+        this.rigid = GetComponent<Rigidbody2D> ();
+        this.player = GameObject.Find ("max");
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (player.transform.localScale.x == 1 && Input.GetKey (KeyCode.A)) {
+            rigid.AddForce (transform.right * moveForce);
+        } else if (player.transform.localScale.x == -1 && Input.GetKey (KeyCode.A)) {
+            rigid.AddForce (transform.right * moveForce * -1);
+        }
+    }
+}
---
+++ Assets/wanGenerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class wanGenerator : MonoBehaviour {
+        GameObject player;
+        this.player = GameObject.Find ("max");
+        // Update is called once per frame
+        void Update () {
+            if (Input.GetKey (KeyCode.A) && player.transform.localScale.x == 1) {
+                GameObject attack = Instiate (arrowPrefab) as GameObject;
+                attack.transform.position = new Vector3 ((player.transform.position.x + 5), 0, 0);
+            } else if (Input.GetKey (KeyCode.A) && player.transform.localScale.x == -1) {
+                GameObject attack = Instiate (arrowPrefab) as GameObject;
+                attack.transform.position = new Vector3 ((player.transform.position.x - 5), 0, 0);
+            }
+        }
