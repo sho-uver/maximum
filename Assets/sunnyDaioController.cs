ac5ef645b417f7c5cd59a5423873b391c34c5794
sho-Uber <shogo1212mail@yahoo.co.jp>
2020-06-17 01:44:04 +0900

    サニー大王

---
+++ Assets/sunnyDaioController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sunnyDaioController : MonoBehaviour {
+    GameObject max;
+    // Start is called before the first frame update
+    void Start () {
+        this.max = GameObject.Find ("max");
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.position = new Vector3 ((max.transform.position.x - 10), 5.5f, 1);
+    }
+}
