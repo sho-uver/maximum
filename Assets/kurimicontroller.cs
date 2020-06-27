3319b27e618fc2fded097b5592d61e55e730ae33
sho-Uber <shogo1212mail@yahoo.co.jp>
2020-06-28 01:04:09 +0900

    リスがクルミを投げるようにする

---
+++ Assets/kurimicontroller.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kurimicontroller : MonoBehaviour {
+    Rigidbody2D rigid;
+    GameObject risu;
+    GameObject max;
+    float moveforce = 1000.0f;
+    // Start is called before the first frame update
+    void Start () {
+        this.rigid = GetComponent<Rigidbody2D> ();
+        this.risu = GameObject.Find ("risurisu");
+        this.max = GameObject.Find ("max");
+        if (max.transform.position.x < risu.transform.position.x) {
+            transform.position = new Vector3 ((risu.transform.position.x - 3), -1, 3);
+            rigid.AddForce (transform.right * -1 * moveforce);
+        } else if (max.transform.position.x > risu.transform.position.x) {
+            transform.position = new Vector3 ((risu.transform.position.x + 3), -1, 3);
+            rigid.AddForce (transform.right * moveforce);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Mathf.Abs (rigid.transform.position.x - risu.transform.position.x) > 10.0f) {
+            Destroy (gameObject);
+        }
+    }
+}
---
+++ Assets/kurumiGenerator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class kurumiGenerator : MonoBehaviour {
+    public GameObject kurumiPrefab;
+
+    void Update () {
+        int count = GameObject.FindGameObjectsWithTag ("kurumi").Length;
+        if (Input.GetKey (KeyCode.A) && count == 0) {
+            GameObject attack = Instantiate (kurumiPrefab) as GameObject;
+        }
+    }
+}
